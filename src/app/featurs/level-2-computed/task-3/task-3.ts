910ac816f0e08f7c34910c7b26fc41e168c025c1
dandreev <Azazello1992Kot!>
2026-02-06 22:32:39 +0300

    Реализовал 3 задачу

--- src/app/featurs/level-2-computed/task-3/task-3.ts
+++ src/app/featurs/level-2-computed/task-3/task-3.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal, WritableSignal } from '@angular/core';
+
+type Item = { id: number; title: string };
+
+function generateItems(count: number): Item[] {
+  const words = ['Angular', 'Signals', 'Computed', 'Memo', 'Filter', 'Search'];
+  return Array.from({ length: count }, (_, i) => {
+    const id = i + 1;
+    const w1 = words[id % words.length];
+    const w2 = words[(id * 3) % words.length];
+    return { id, title: `${w1} ${w2} item #${id}` };
+  });
+}
 
 @Component({
   selector: 'app-task-3',
@@ -7,5 +19,45 @@ import { Component } from '@angular/core';
   styleUrl: './task-3.css',
 })
 export class Task3 {
+  readonly items = signal<Item[]>(generateItems(2000));
+  readonly query = signal('');
 
+  readonly recomputeCount = signal(0);
+
+  // ❗TODO: сделать computed filtered
+  // Требования:
+  // - Внутри увеличивать recomputeCount (update +1)
+  // - Фильтрация по query (case-insensitive)
+  // - Если query пустая => вернуть исходный список items()
+  readonly filtered = computed(() => {
+    const q = this.query().trim().toLowerCase();
+
+    if (q === '') {
+      return this.items();
+    }
+
+    return this.items().filter((item) => item.title.toLowerCase().includes(q));
+  });
+
+  constructor() {
+    effect(() => {
+      this.filtered();
+      this.recomputeCount.update((n) => n + 1);
+    });
+  }
+
+  setQuery(v: string) {
+    this.query.set(v);
+  }
+
+  replaceItems() {
+    this.items.set([...this.items().reverse()]);
+  }
+
+  trackById = (_: number, it: Item) => it.id;
+
+  reading() {
+    // ❗TODO: обратите внимание на recomputeCount. Счетчик не меняется.
+    console.log(this.filtered());
+  }
 }
